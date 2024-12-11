5a4f329d0f977ae4a4b1e3afb088601d7cfd3da6
Meet Bodana <meetbodana@192.168.1.14>
2024-12-11 22:11:39 +0530

    Initial commit: AI Chatbot app setup

---
+++ routes/chatBot.routes.js
@@ -0,0 +1,31 @@
+import Groq from "groq-sdk";
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const groq = new Groq({
+  apiKey: process.env.GROQ_KEY,
+});
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.render("chatArea");
+});
+
+router.post("/send", async (req, res) => {
+  const message = req.body.message.trim();
+  const completion = await groq.chat.completions.create({
+    messages: [
+      {
+        role: "user",
+        content: message,
+      },
+    ],
+    model: "llama3-8b-8192",
+  });
+
+  const response = completion.choices[0].message.content;
+  res.send(response);
+});
+
+export default router;
